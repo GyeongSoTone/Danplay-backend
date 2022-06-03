4dedef38bb2df2b8d318555c7096c43842c3a515
HongdeokKim <qchdkim@naver.com>
2022-06-04 01:48:36 +0900

    feat: 오류 코드에 대한 레거시 정리, 상태 코드 재정의

--- src/main/java/com/danplay/server/base/exception/DanplayException.java
+++ src/main/java/com/danplay/server/base/exception/DanplayException.java
@@ -1,6 +1,7 @@
 package com.danplay.server.base.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public class DanplayException extends RuntimeException {
@@ -8,10 +9,12 @@ public class DanplayException extends RuntimeException {
     private final ExceptionCodeAndDetails codeAndMessage = ExceptionCodeAndDetails
             .findByClass(this.getClass());
 
+    private HttpStatus status;
     private String code;
     private String message;
 
     public DanplayException() {
+        this.status = codeAndMessage.getStatus();
         this.code = codeAndMessage.getCode();
         this.message = codeAndMessage.getMessage();
     }
--- src/main/java/com/danplay/server/base/exception/ExceptionCodeAndDetails.java
+++ src/main/java/com/danplay/server/base/exception/ExceptionCodeAndDetails.java
@@ -3,12 +3,11 @@ package com.danplay.server.base.exception;
 import com.danplay.server.mail.exception.DuplicateMailException;
 import com.danplay.server.user.exception.InvalidFindPasswordException;
 import com.danplay.server.mail.exception.InvalidMailCodeException;
-import com.danplay.server.mail.exception.InvalidMailTypeException;
-import com.danplay.server.user.exception.DuplicateLoginIdException;
 import com.danplay.server.user.exception.InvalidPasswordException;
 import com.danplay.server.user.exception.NonExistMailUserException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.util.Arrays;
@@ -16,15 +15,15 @@ import java.util.Arrays;
 @Getter
 @AllArgsConstructor
 public enum ExceptionCodeAndDetails {
-    NOT_FOUND_API("0002", "해당 경로에 대한 응답 API를 찾을 수 없습니다", NoHandlerFoundException.class),
-    INVALID_REQUEST_TYPE("0003", "요청한 API에서 요구하는 형식에 맞지 않는 필드가 존재합니다", InvalidRequestTypeException.class),
-    INVALID_MAIL_TYPE("1001", "형식에 맞지 않는 메일입니다", InvalidMailTypeException.class),
-    DUPLICATE_USER("1002", "Danplay 서비스에 가입되어있는 계정입니다", DuplicateMailException.class),
-    INVALID_MAIL_CODE("1003", "유효하지 않은 인증코드 입니다", InvalidMailCodeException.class),
-    NON_EXIST_MAIL_USER("2001", "존재하지 않는 계정입니다", NonExistMailUserException.class),
-    INVALID_PASSWORD("2002","비밀번호가 일치하지 않습니다", InvalidPasswordException.class),
-    INVALID_FIND_PASSWORD("2003", "서버에 저장된 사용자 정보와 일치하지 않습니다", InvalidFindPasswordException.class);
+    NOT_FOUND_API(HttpStatus.NOT_FOUND, "0002", "해당 경로에 대한 응답 API를 찾을 수 없습니다", NoHandlerFoundException.class),
+    INVALID_REQUEST_TYPE(HttpStatus.BAD_REQUEST, "0003", "요청한 API에서 요구하는 형식에 맞지 않는 필드가 존재합니다", InvalidRequestTypeException.class),
+    DUPLICATE_USER(HttpStatus.CONFLICT, "1001", "Danplay 서비스에 가입되어있는 계정입니다", DuplicateMailException.class),
+    INVALID_MAIL_CODE(HttpStatus.CONFLICT, "1002", "유효하지 않은 인증코드 입니다", InvalidMailCodeException.class),
+    NON_EXIST_MAIL_USER(HttpStatus.NOT_FOUND, "2001", "존재하지 않는 계정입니다", NonExistMailUserException.class),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "2002","비밀번호가 일치하지 않습니다", InvalidPasswordException.class),
+    INVALID_FIND_PASSWORD(HttpStatus.UNAUTHORIZED, "2003", "서버에 저장된 사용자 정보와 일치하지 않습니다", InvalidFindPasswordException.class);
 
+    private final HttpStatus status;
     private final String code;
     private final String message;
     private final Class<? extends Exception> type;
